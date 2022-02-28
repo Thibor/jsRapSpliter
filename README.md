042cf54f6d2ef0c9e08b522cf50f619314864918
Thibor <thibor@vp.pl>
2022-02-28 16:18:55 +0000

    add spliter veritical

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ Spliter jquery plugin
 
 #### Demo
 
-[https://thibor.github.io/jsRapStopwatch/](https://thibor.github.io/jsRapSpliter/) 
+[https://thibor.github.io/jsRapStopwatch/](https://thibor.github.io/jsRapSpliter/)
+
+### Settings
+
+Option | Type | Default | Description
+------ | ---- | ------- | -----------
+height | string | '200px' | Height of main div
+horizontal | bool | true | Orientation of spliter
+position | float | 0.5 | Size of first panel
+spliterWidth | float | 8.0 | Width of spliter bar
--- jsRapSpliter.js
+++ jsRapSpliter.js
@@ -1,19 +1,36 @@
+$(window).resize(function () {
+	$('.rapSpliter').each(function () {
+		this.Render();
+	});
+});
+
 (function ($) {
 	$.fn.jsRapSpliter = function (options) {
 
 		return this.each(function () {
-			let position = $(this).width() / 2.0;
 			this.opt = $.extend({
-				position: position
+				height: '200px',
+				horizontal: true,
+				position: 0.5,
+				spliterWidth: 4
 			}, options);
 			let base = this;
 			let active = false;
-			$(this).addClass('rapSpliter');
-			let panel1 = $('.rapSpliter div:first').addClass('panel1')[0];
-			let panel2 = $('.rapSpliter div:last').addClass('panel2')[0];
-			let spliter = $('<div>').addClass('spliter')[0];
-			$(panel1).after(spliter);
-			$(spliter).bind({
+			$(this).addClass('rapSpliter').css({ height: this.opt.height });
+			this.panel1 = $('div:first', this)[0];
+			this.panel2 = $('div:last', this)[0];
+			this.spliter = $('<div>')[0];
+			if (this.opt.horizontal) {
+				$(this.panel1).addClass('panel1H');
+				$(this.panel2).addClass('panel2H');
+				$(this.spliter).addClass('spliterH');
+			} else {
+				$(this.panel1).addClass('panel1V');
+				$(this.panel2).addClass('panel2V');
+				$(this.spliter).addClass('spliterV');
+			}
+			$(this.panel1).after(this.spliter);
+			$(this.spliter).bind({
 				mousedown: function (e) {
 					active = e.buttons == 1;
 				}
@@ -21,32 +38,67 @@
 			$(this).bind({
 				mousemove: function (e) {
 					if (active)
-						UpdatePosition(e);
+						base.UpdatePosition(e);
 				},
 				mouseup: function (e) {
 					active = false;
 				}
 			});
 
-			function UpdatePosition(e) {
-				let p = e.clientX - $(base)[0].getBoundingClientRect().left;
-				base.SetPosition(p);
+			this.UpdatePosition = function (e) {
+				if (this.opt.horizontal) {
+					let ph = (e.clientX - $(base)[0].getBoundingClientRect().left) / $(base).width();
+					this.SetPositionH(ph);
+				} else {
+					let pv = (e.clientY - $(base)[0].getBoundingClientRect().top) / $(base).height();
+					this.SetPositionV(pv);
+				}
+			}
+
+			this.SetPositionH = function (pro) {
+				base.opt.position = pro;
+				let w = $(base).width();
+				let hsw = base.opt.spliterWidth / 2.0;
+				let pos = w * pro;
+				if (pos < hsw)
+					pos = hsw;
+				if (pos > w - hsw)
+					pos = w - hsw;
+				let x1 = pos - hsw;
+				let x2 = w - pos - hsw;
+				$(base.panel1).width(x1);
+				$(base.panel2).width(x2);
+				$(base.spliter).css({ left: x1 + 'px', right: x2 + 'px' });
+			}
+
+			this.SetPositionV = function (pro) {
+				base.opt.position = pro;
+				let h = $(base).height();
+				let hsw = base.opt.spliterWidth / 2.0;
+				let pos = h * pro;
+				if (pos < hsw)
+					pos = hsw;
+				if (pos > h - hsw)
+					pos = h - hsw;
+				let y1 = pos - hsw;
+				let y2 = h - pos - hsw;
+				$(base.panel1).height(y1);
+				$(base.panel2).height(y2);
+				$(base.spliter).css({ top: y1 + 'px', bottom: y2 + 'px' });
 			}
 
 			this.SetPosition = function (pos) {
-				let w = $(base).width();
-				if (pos < 10)
-					pos = 10;
-				if (pos > w - 10)
-					pos = w - 10;
-				let x1 = pos - 2;
-				let x2 = w - pos - 2;
-				$('.panel1').width(x1);
-				$('.panel2').width(x2);
-				$('.spliter').css({ left: x1 + 'px', right: x2 + 'px' });
+				if (this.opt.horizontal)
+					this.SetPositionH(pos);
+				else
+					this.SetPositionV(pos);
 			}
 
-			this.SetPosition(this.opt.position);
+			this.Render = function () {
+				this.SetPosition(this.opt.position);
+			}
+
+			this.Render();
 
 		})
 
